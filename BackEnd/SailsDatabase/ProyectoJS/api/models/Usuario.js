85a05fcd0e05fd58ebdb1fd8b46beb4c078d1f83
Danni <daniebro.5@gmail.com>
2018-07-22 14:59:39 -0500

    base terminada

--- BackEnd/SailsDatabase/ProyectoJS/api/models/Usuario.js
+++ BackEnd/SailsDatabase/ProyectoJS/api/models/Usuario.js
@@ -9,19 +9,40 @@ module.exports = {
 
   attributes: {
 
-    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
-    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
-    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    nombreCompletoUsuario: {
+      type: 'string',
+      required: true,
+      description: 'Representación completa del nombre del usuario',
+      example: 'Abradolf Lincler'
+    },
+    correoUsuario: {
+      type: 'string',
+      required: true,
+      unique: true,
+      isEmail: true,
+      maxLength: 200,
+      example: 'nombre.apellido@ejemplo.com'
+    },
 
+    passwordUsuario: {
+      type: 'string',
+      required: true,
+      description: 'Se guarda el hash de la contrasenia',
+      protect: true,
+      example: '2$28a8eabna301089103-13948134nad'
+    },
 
-    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
-    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
-    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+    imagenUsuario: {
+      type: 'string',
+      required: false,
+      description: 'imagen del usuario para mostrar',
+      example: 'Abradolf Lincler'
+    },
 
-
-    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
-    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    lugares: {
+      collection: 'Lugar',
+      via: 'usuarioLugar'
+    }
 
   },
 
